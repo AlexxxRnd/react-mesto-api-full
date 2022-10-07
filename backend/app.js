9b44461e9ac0dd1a5d4caab1b0083bd9c7206793
Alex <alexgarbuzov@ya.ru>
2022-10-07 17:36:44 +0300

    1

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://alexr.students.nomoredomains.icu/',
+  origin: "*",
 }))
 
 app.use(requestLogger);
