ce1aaae0511733c71b5cacc46b01bd4427a6c240
Alex <alexgarbuzov@ya.ru>
2022-10-08 23:25:44 +0300

    1

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-
+const { NODE_ENV, JWT_SECRET } = process.env;
 const BadRequestError = require('../errors/BadRequestError');
 const NotFoundError = require('../errors/NotFoundError');
 const ConflictError = require('../errors/ConflictError');
