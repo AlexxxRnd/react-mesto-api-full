c591d343eb04fcae89ad84238d8223e7ccf91caf
Alex <alexgarbuzov@ya.ru>
2022-10-08 22:20:09 +0300

    1

--- backend/app.js
+++ backend/app.js
@@ -19,8 +19,10 @@ const {
 const allowedCors = [
   'http://alexr.students.nomoredomains.icu',
   'https://alexr.students.nomoredomains.icu',
-  'localhost:3000'
+  'http://localhost:3000',
+  'https://localhost:3000',
 ];
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 const { PORT = 3000 } = process.env;
 
@@ -35,8 +37,16 @@ app.use(express.json());
 
 app.use(function (req, res, next) {
   const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  res.header('Access-Control-Allow-Credentials', true);
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
   }
   next();
 });
