b02183d392854b1196cba66e2d564eaae842fb9e
Alex <alexgarbuzov@ya.ru>
2022-10-08 23:59:13 +0300

    1

--- backend/app.js
+++ backend/app.js
@@ -17,12 +17,12 @@ const {
   login,
 } = require('./controllers/user');
 
-const allowedCors = [
-  'http://alexr.students.nomoredomains.icu',
-  'https://alexr.students.nomoredomains.icu',
-  'http://localhost:3000',
-  'https://localhost:3000',
-];
+// const allowedCors = [
+//   'http://alexr.students.nomoredomains.icu',
+//   'https://alexr.students.nomoredomains.icu',
+//   'http://localhost:3000',
+//   'https://localhost:3000',
+// ];
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 const { PORT = 3000 } = process.env;
@@ -36,22 +36,22 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  const { origin } = req.headers;
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  res.header('Access-Control-Allow-Credentials', true);
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-  next();
-});
-
+// app.use(function (req, res, next) {
+//   const { origin } = req.headers;
+//   const { method } = req;
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   res.header('Access-Control-Allow-Credentials', true);
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     return res.end();
+//   }
+//   next();
+// });
+app.use(cors());
 app.use(requestLogger);
 
 app.get('/crash-test', () => {
